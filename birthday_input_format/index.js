8dc5b1459620820081dcb2c5b7a0b3702b8b57e7
Nami5011 <maron.011821@gmail.com>
2024-10-09 19:58:23 +0900

    date formatter

---
+++ birthday_input_format/index.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  document.getElementById('birthday_ja').addEventListener('input', function (e) {
+    let value = e.target.value;
+    const lastChar = value.slice(-1);
+    if (lastChar === '/') {
+      e.target.value = value.replace(/[0-9]\s\/$/, '');
+      return;
+    }
+    const reg_2b_num = /[０-９]/g;
+    if (reg_2b_num.test(value)) {
+      e.target.value = value = value.replace(reg_2b_num, function(num) {
+        return String.fromCharCode(num.charCodeAt(0) - 0xFEE0);
+      })
+    }
+    const digits = e.target.value.replace(/\D/g, '').match(/(\d{0,4})(\d{0,2})(\d{0,2})/);
+    e.target.value = digits[1].length < 4 ? digits[1] : digits[1] + ' / ' + digits[2] + (digits[2].length === 2 ? ' / ' + digits[3] : '');
+  });
+});
