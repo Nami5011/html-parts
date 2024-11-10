c9acf18af8fc88d2e5fe9d18ef1634c6bf7ccec3
Nami5011 <maron.011821@gmail.com>
2024-11-10 12:59:56 +0900

    popup animation and responsive

--- popup/index.js
+++ popup/index.js
@@ -23,21 +23,30 @@ function closePopup() {
   document.body.style.overflow = "visible";
 }
 
-function openPopup(event) {
-  setPopupContent(event);
+function openPopup(elem) {
+  setPopupContent(elem);
+  // Set the clicked card's location
+  const rect = elem.getBoundingClientRect();
+  document.documentElement.style
+    .setProperty('--popup-origin-top', (rect.top + (rect.height / 2)) + 'px');
+  document.documentElement.style
+    .setProperty('--popup-origin-left', (rect.left + (rect.width / 2)) + 'px');
+  // Display the popup
   const modal = document.getElementById("popup");
   modal.style.display = "block";
+  // Prevent to scroll under the popup
   document.body.style.overflow = "hidden";
 }
 
-function setPopupContent(elm) {
- const id = elm.dataset.id;
- const img = document.querySelector(`[data-id="${id}"] img`).getAttribute('src');
- const name = document.querySelector(`[data-id="${id}"] .card-header .title`).textContent;
- const rating = document.querySelector(`[data-id="${id}"] .card-header .rating`).textContent;
- const description = document.querySelector(`[data-id="${id}"] .card-body`).textContent;
- document.querySelector(`#popup img`).setAttribute('src', img);
- document.querySelector(`#popup .popup-content-left .room-name`).textContent = name;
- document.querySelector(`#popup .popup-content-left .award-rating`).textContent = rating;
- document.querySelector(`#popup .popup-content-left .description`).textContent = description;
+function setPopupContent(elem) {
+  // Set the variables
+  const id = elem.dataset.id;
+  const img = document.querySelector(`[data-id="${id}"] img`).getAttribute('src');
+  const name = document.querySelector(`[data-id="${id}"] .card-header .title`).textContent;
+  const rating = document.querySelector(`[data-id="${id}"] .card-header .rating`).textContent;
+  const description = document.querySelector(`[data-id="${id}"] .card-body`).textContent;
+  document.querySelector(`#popup img`).setAttribute('src', img);
+  document.querySelector(`#popup .popup-content-left .room-name`).textContent = name;
+  document.querySelector(`#popup .popup-content-left .award-rating`).textContent = rating;
+  document.querySelector(`#popup .popup-content-left .description`).textContent = description;
 }
